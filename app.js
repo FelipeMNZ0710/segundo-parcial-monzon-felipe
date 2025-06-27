86fbb7c20088ee432a54cf9525f7a736190ec3f3
Felipe <felipemonzon0710@gmail.com>
2025-06-27 10:14:50 -0300

    configuración inicial del servidor, variables de entorno y script de arranque

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import bookRoutes from './src/routes/bookRoutes.js';
+import sequelize from './src/config/database.js';
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use('/api', bookRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  });
+});
